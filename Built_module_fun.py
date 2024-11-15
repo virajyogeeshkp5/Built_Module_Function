d1769787416d2effee4c1d4d67a62e5fd4f3e690
YOGEESH K P <yogeesh793@gmail.com>
2024-11-15 11:22:51 +0530

    Add files via upload

---
+++ Built_module_fun.py
@@ -0,0 +1,20 @@
+print(pow(2,3))
+
+print(dir(__builtins__))
+
+print(help(max))
+
+import math
+print(math.sqrt(16))
+
+import math
+print(dir(math))
+
+print(math.__doc__)
+
+def sayhello():
+    """this is the function of docstring"""
+    return 'hello world'
+
+import hello world
+print(hello world.__doc__)
